af527d2cc6fd6c5901fe6886a20ceec77b6ce46b
Jackson Morgan <jmorgan@stuy.edu>
2016-12-11 17:17:35 -0500

    why windows just why

--- write.c
+++ write.c
@@ -17,8 +17,11 @@ int main(){
   if(1){// insert code to check semaphone
     // insert code to read from file
     int fd = open( "text.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    int k = 0; // placeholder. want k = to negative shared memory
+    lseek(fd, , SEEK_END);
     // read using size of last line
     // write new info
     //update shared memory with length of new line
+  }
   return 0;
 }
