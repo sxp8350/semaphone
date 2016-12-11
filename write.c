3aa7c9b7e1e921fedd546688a813bd5b28b26363
Jackson Morgan <jmorgan@stuy.edu>
2016-12-11 17:08:54 -0500

    a plan

--- write.c
+++ write.c
@@ -14,5 +14,11 @@
 
 
 int main(){
+  if(1){// insert code to check semaphone
+    // insert code to read from file
+    int fd = open( "text.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    // read using size of last line
+    // write new info
+    //update shared memory with length of new line
   return 0;
 }
