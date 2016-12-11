40efc0f3dfb0ee924b94e2f97b3b1b54f4e74abf
Jackson Morgan <jmorgan@stuy.edu>
2016-12-11 18:16:40 -0500

    it would be helpful if we could check our work

--- write.c
+++ write.c
@@ -20,12 +20,14 @@ int main(){
     int fd = open( "text.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
     int k = 0; // placeholder. want k = to negative shared memory
     lseek(fd,k, SEEK_END);
+    // read using size of last line
     printf("previous line is: \n");
     printf("Enter new line: \n");
     fgets(a,sizeof(a),stdin);
-    // read using size of last line
     // write new info
-    //update shared memory with length of new line
+    int f = strlen(a); //update shared memory with length of new line
+    printf("%d \n", f);
+    
   }
   return 0;
 }
