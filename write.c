780ec23890fdcf78be91aedabb8f9cfb53b6b061
Jackson Morgan <jmorgan@stuy.edu>
2016-12-11 17:22:01 -0500

    dasdaD

--- write.c
+++ write.c
@@ -18,7 +18,7 @@ int main(){
     // insert code to read from file
     int fd = open( "text.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
     int k = 0; // placeholder. want k = to negative shared memory
-    lseek(fd, , SEEK_END);
+    lseek(fd,k, SEEK_END);
     // read using size of last line
     // write new info
     //update shared memory with length of new line
